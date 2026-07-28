95b4be4a0cd65899d6b8bb27f8d0d124cbe13749
Paria Soleimani <paria.slmni@gmail.com>
2026-07-28 20:51:49 +0330

    chore: update app name

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { getDefaultConfig } from 'expo/metro-config.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const config = getDefaultConfig(__dirname);
