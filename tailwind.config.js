7e45403cf0dcf61f325f63dd7eb62dc6f6cc08ac
Paria Soleimani <paria.slmni@gmail.com>
2026-07-28 20:43:02 +0330

    chore: update configurations and remove unused dependencies

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/components/**/*.{js,jsx}', './src/screens/**/*.{js,jsx}', './src/app/**/*.{js,jsx}'],
-	presets: [require('nativewind/preset')],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
